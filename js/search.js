0edcf6e5907a6f04607860f43cd64eb1c22fb30b
jiggy1com <joe@jiggy1.com>
2015-12-03 15:08:41 -0800

    javascript for search

---
+++ js/search.js
@@ -0,0 +1,146 @@
+'use strict';
+
+// Search Constructor
+function Search(){
+	// this.prop = 'value';
+}
+
+// Search Prototypes
+
+Search.prototype.clearSearchResults = function(){
+	// hide alert
+	$('#errorMsg').html('test').hide();
+
+	// hide results container
+	$('#search-results-container').hide();
+
+	// clear results list
+	$('#search-results').html('');
+}
+
+Search.prototype.runSearch = function(){
+	var self = this; // allow internal access
+
+	// clear results & errors
+	this.clearSearchResults();
+
+	// get results
+	$.ajax({
+		url : 'ajax.php',
+		method : 'POST',
+		dataType : 'json',
+		data : {
+			criteria : $('#criteria').val(),
+			matchType : $('input:radio[name=matchType]:checked').val()
+		}
+	})
+	.success(function(data){
+		self.displayResults(data);
+	})
+	.error(function(){
+		self.showAjaxError();
+	});
+}
+
+Search.prototype.displayResults = function(obj){
+	if(obj.errorMsg != ''){
+		$('#errorMsg').html(obj.errorMsg).slideDown();
+	}else{
+		// fill in results
+		for(var i=0;i<obj.items.length;i++){
+			$('#search-results').append('<li>' + obj.items[i] + '</li>');
+		}
+		// show results
+		$('#search-results-container').slideDown();
+	}
+}
+
+Search.prototype.showAjaxError = function(){
+	$('#errorMsg').html('An unknown error occurred. Please try again.').slideDown();
+}
+
+Search.prototype.init = function(){
+	var self = this; // allow internal access
+	$('#btn-search').click(function(){
+		self.runSearch();
+	});
+}
+
+// fire up search when DOM is ready
+$(document).ready(function(){
+	var search = new Search();
+		search.init();
+});
+	
+
+/* old code
+
+var Search = function(){
+
+	var self = this;
+
+	this.clearSearchResults = function(){
+		// hide alert
+		$('#errorMsg').hide();
+
+		// hide results container
+		$('#search-results-container').hide();
+
+		// clear results list
+		$('#search-results').html('');
+	}
+
+	this.runSearch = function(){
+
+		self.clearSearchResults();
+
+		// get results
+		$.ajax({
+			url : 'ajax.php',
+			method : 'POST',
+			dataType : 'json',
+			data : {
+				criteria : $('#criteria').val(),
+				matchType : $('input:radio[name=matchType]:checked').val()
+			}
+		})
+		.success(function(data){
+			self.displayResults(data);
+		})
+		.error(function(){
+			// ?
+		});
+	}
+
+	this.displayResults = function(obj){
+		if(obj.errorMsg != ''){
+			$('#errorMsg').html(obj.errorMsg).slideDown();
+		}else{
+			// fill in results
+			for(var i=0;i<obj.items.length;i++){
+				$('#search-results').append('<li>' + obj.items[i] + '</li>');
+			}
+			// show results
+			$('#search-results-container').slideDown();
+		}
+	}
+
+	this.init = function(){
+		$('#btn-search').click(function(){
+			self.runSearch();
+		});
+	};
+
+	return {
+		init : self.init,
+		runSearch : self.runSearch,
+	}
+
+}
+
+$(document).ready(function(){
+	var search = new Search();
+		search.init();
+});
+
+*/
